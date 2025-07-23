f4131e2e45097f7cbf50193cfd7bcb768e041646
Ricardo Perez <ricardo.pereze.98@gmail.com>
2025-07-23 11:58:38 -0400

    [FETCH](res user) fix _init_messaging to rechive store

--- mail_gateway/models/res_users.py
+++ mail_gateway/models/res_users.py
@@ -9,7 +9,14 @@ class ResUsers(models.Model):
 
     gateway_ids = fields.Many2many("mail.gateway")
 
-    def _init_messaging(self):
-        result = super()._init_messaging()
-        result["gateways"] = self.gateway_ids.gateway_info()
+    def _init_messaging(self, store):
+        """Extend messaging initialization to inject gateway info.
+
+        Starting from Odoo 18.0 ``_init_messaging`` receives a ``store``
+        object where data must be added using :py:meth:`store.add` instead of
+        returning a dict.  This overrides the upstream implementation while
+        preserving compatibility with the new signature.
+        """
+        result = super()._init_messaging(store)
+        store.add({"gateways": self.gateway_ids.gateway_info()})
         return result
